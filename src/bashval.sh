59fd1b9ccf5384d31082e14f6b918385e653b8b3
lafkpages <soy.lafk+gh@gmail.com>
2023-06-26 15:52:34 +0200

    Encode decode funcs take arg

--- src/bashval.sh
+++ src/bashval.sh
@@ -7,11 +7,11 @@ then
 fi
 
 encode() {
-  protoc --encode=api.Command "$proto"
+  protoc --encode="${$1:-api.Command}" "$proto"
 }
 
 decode() {
-  protoc --decode=api.Command "$proto"
+  protoc --decode="${$1:-api.Command}" "$proto"
 }
 
 toast() {
