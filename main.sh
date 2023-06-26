7b8989a051872f43224a4f2f91ad0a4606be7c2f
lafkpages <soy.lafk+gh@gmail.com>
2023-06-26 16:04:57 +0200

    Pass PATH to websocketd

--- main.sh
+++ main.sh
@@ -8,4 +8,4 @@ if [ ! -f "$proto" ] || [ ! -f "$proto_client" ]; then
   exit 1
 fi
 
-websocketd --port=4096 --devconsole -binary --passenv proto,proto_client ./src/bashval.sh
+websocketd --port=4096 --devconsole -binary --passenv proto,proto_client,PATH ./src/bashval.sh
