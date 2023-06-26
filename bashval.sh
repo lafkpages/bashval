9b8a829e740f36cf88b122c17791a9a98f0797d9
lafkpages <soy.lafk+gh@gmail.com>
2023-06-26 14:27:31 +0200

    Missing protoc warning

--- bashval.sh
+++ bashval.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if ! command -v protoc &> /dev/null
+then
+  echo "protoc not found, please download from https://github.com/protocolbuffers/protobuf/releases/" 1>&2
+  exit 1
+fi
+
 if [ ! -f "api.proto" ]; then
   echo "Missing protobuf files, please download them from https://govaldocs.pages.dev/api.proto" 1>&2
   exit 1
