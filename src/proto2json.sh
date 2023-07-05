19e8f121e867b869488a503f23a6ecb03db9bb47
lafkpages <soy.lafk+gh@gmail.com>
2023-07-05 02:34:30 +0200

    proto2json util

---
+++ src/proto2json.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+proto=`cat`
+
+json='{'
+
+for token in $proto; do
+  if [[ "$token" =~ ([a-zA-Z]+): ]]; then
+    json="$json\"${BASH_REMATCH[1]}\":"
+    continue
+  fi
+
+  case "$token" in 
+    '{')
+      json="$json:{"
+      ;;
+    '}')
+      json="$json},"
+      ;;
+    ':')
+      json="$json:"
+      ;;
+    *)
+      json="$json\"$token\""
+      ;;
+  esac
+done
+
+json="$json}"
+
+# Remove trailing comma
+json=`sed -e 's/,}$/}/' <<< "$json"`
+
+# Output JSON
+echo "$json" | jq -cM .
