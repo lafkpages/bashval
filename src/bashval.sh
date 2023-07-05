d80bbadf8ec48ba43566a29af0e217b297454b37
lafkpages <soy.lafk+gh@gmail.com>
2023-07-05 16:16:52 +0200

    Move chan0 service back into main loop

--- src/bashval.sh
+++ src/bashval.sh
@@ -63,12 +63,45 @@ while IFS='$\n' read -r line; do
   ref=$(jq -Mr .ref <<< "$msg")
 
   # Message channel
-  _chan="$(jq -Mr .channel <<< "$msg")"
-  chan="${_chan:-0}"
+  chan="$(jq -Mr .channel <<< "$msg")"
+  if [ "$chan" = "null" ] || [ -z "$chan" ]; then
+    chan="0"
+  fi
 
   # Do something with the message
   if [ "$chan" = "0" ]; then
-    chan0 "$ref" <<< "$msg"
-    continue
+    if [ ! "$(jq -M .ping <<< "$msg")" = "null" ]; then
+      encode <<- EOM
+pong {}
+ref: "$ref"
+EOM
+    elif [ ! "$(jq -M .openChan <<< "$msg")" = "null" ]; then
+      # Get channel service
+      service="$(jq -Mrc .openChan.service <<< "$msg")"
+
+      # Find next available channel ID
+      # (lastChanId plus one)
+      chanId="$((lastChanId + 1))"
+
+      # Logs
+      echo -n "Opening channel $chanId" 1>&2
+      echo -n $'\t' 1>&2
+      echo "with service $service" 1>&2
+
+      # Increment the last used channel ID
+      lastChanId="$chanId"
+
+      # Save channel
+      channels["$chanId"]="$(jq -Mrc .openChan <<< "$msg")"
+
+      # Send response
+      encode <<- EOM
+session: 1
+openChanRes {
+  id: $chanId
+}
+ref: "$ref"
+EOM
+    fi
   fi
 done
