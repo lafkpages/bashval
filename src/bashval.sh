a1b61cce9a7d1933495047a36fa78fff01c8ab35
lafkpages <soy.lafk+gh@gmail.com>
2023-07-05 14:15:36 +0200

    Move to chan0 service (broken)

--- src/bashval.sh
+++ src/bashval.sh
@@ -17,6 +17,11 @@ toast {
 EOM
 }
 
+# Import services
+for service in src/services/*.sh; do
+  source "$service"
+done
+
 # Initial ready message
 encode <<- EOM
 containerState {
@@ -57,38 +62,13 @@ while IFS='$\n' read -r line; do
   # Message ref
   ref=$(jq -Mr .ref <<< "$msg")
 
+  # Message channel
+  _chan="$(jq -Mr .channel <<< "$msg")"
+  chan="${_chan:-0}"
+
   # Do something with the message
-  if [ ! "$(jq -M .ping <<< "$msg")" = "null" ]; then
-    encode <<- EOM
-pong {}
-ref: "$ref"
-EOM
-  elif [ ! "$(jq -M .openChan <<< "$msg")" = "null" ]; then
-    # Get channel service
-    service="$(jq -Mrc .openChan.service <<< "$msg")"
-
-    # Find next available channel ID
-    # (lastChanId plus one)
-    chanId="$((lastChanId + 1))"
-
-    # Logs
-    echo -n "Opening channel $chanId" 1>&2
-    echo -n $'\t' 1>&2
-    echo "with service $service" 1>&2
-
-    # Increment the last used channel ID
-    lastChanId="$chanId"
-
-    # Save channel
-    channels["$chanId"]="$(jq -Mrc .openChan <<< "$msg")"
-
-    # Send response
-    encode <<- EOM
-session: 1
-openChanRes {
-  id: $chanId
-}
-ref: "$ref"
-EOM
+  if [ "$chan" = "0" ]; then
+    chan0 "$ref" <<< "$msg"
+    continue
   fi
 done
