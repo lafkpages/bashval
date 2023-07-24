60a50326173964aa7a2c62afc7b86d113a76e78d
lafkpages <soy.lafk+gh@gmail.com>
2023-07-24 18:06:05 +0900

    ✅ Dupe keys script

--- test/dups.sh
+++ test/dups.sh
@@ -1,35 +1,7 @@
 #!/usr/bin/env bash
 
-jq -Mcn --stream 'def fromstream_with_dups(i; fix):
-  foreach i as $i (
-    [null, null];
+# Example usage:
+#
+# ./test/dups.sh < test/demo_exec_message_conv.json
 
-    if ($i | length) == 2 then
-      if ($i[0] | length) == 0 then .
-      elif $i[0][-1]|type == "string" then
-        [ ( .[0] | setpath($i[0]; getpath($i[0]) + [$i[1]]) ), .[1] ]
-      else [ ( .[0] | setpath($i[0]; $i[1]) ), .[1] ]
-      end
-    elif ($i[0] | length) == 1 then [ null, .[0] ]
-    else .
-    end;
-
-    if ($i | length) == 1 then
-      if ($i[0] | length) == 1 then .[1] | fix
-      else empty
-      end
-    elif ($i[0] | length) == 0 then $i[1]
-    else empty
-    end
-  );
-def fix:
-  if type != "object" then .
-  else
-    reduce keys_unsorted[] as $key (.;
-      if .[$key]|length == 1 then
-        .[$key] |= .[0]
-      else
-        .
-      end)
-  end;
-fromstream_with_dups(inputs; fix)'
+jq -Mnc --stream -f test/dups.jq
